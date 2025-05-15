48e38f0d0736df869b6e8b3382b702fbc232d615
Vijaya Bhaskar V <vijayabhaskarv@nothing.attdns.com>
2025-05-15 14:14:12 +0530

    Organising the files into Ratelimiter folder

--- build-docker.sh
+++
@@ -1,1 +0,0 @@
-docker build -t rate-limiter:latest .
--- restart-ratelimiter-deployment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl delete deployment -n rate-limiter rate-limiter
-kubectl apply -f local/deployment-local.yaml
-sleep 5
-kubectl get pods -n rate-limiter
